08f1268c74a5c0ef2326938693e5656d8181049f
MrinmoySonowal <M.Sonowal@sms.ed.ac.uk>
2023-09-04 03:57:51 +0100

    Updated the tests to handle parallel clients

--- backend/coin_server.py
+++ backend/coin_server.py
@@ -7,14 +7,17 @@ app = FastAPI()
 BASE_URL = "https://api.exchange.coinbase.com/"
 ALL_PRODUCTS_EXTENSION = "products"
 
+
 def get_all_ids(all_products_response: str) -> List[str]:
     ids = []
     for json_dicts in all_products_response:
         ids.append(json_dicts["id"])
     return ids
 
+
 def get_product_ticker(product_id: str) -> List[float]:
     pass
+
 
 @app.get("/")
 async def root():
@@ -27,4 +30,3 @@ async def root():
         raise HTTPException(status_code=404, detail="Unable to reach URL.")
     else:
         raise HTTPException(status_code=500, detail="Unable to connect API or retrieve data.")
-
--- backend/tests/client_test.py
+++ backend/tests/client_test.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import unittest
 from example_client import get_product_ids
 
@@ -17,6 +18,16 @@ class TestClientServer(unittest.TestCase):
     def test_high_load_on_server(self):
         ids = [set(get_product_ids()) for _ in range(20)]
         self.assertTrue(all([id_list == ids[0] for id_list in ids]))
+
+    def test_parallel_clients(self):
+        num_clients = 5
+        with concurrent.futures.ThreadPoolExecutor(max_workers=num_clients) as executor:
+            results = [executor.submit(get_product_ids) for _ in range(num_clients)]
+            ids = []
+            for f in concurrent.futures.as_completed(results):
+                ids.append(set(f.result()))
+            self.assertEqual(len(ids), 5, "Expected 5 client results!")
+            self.assertTrue(all([id_list == ids[0] and len(id_list) == len(ids[0]) for id_list in ids]))
 
 
 if __name__ == "__main__":
