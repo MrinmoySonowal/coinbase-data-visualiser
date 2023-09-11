a53a8c253cae9e4b51492db938a1c0ee79590ef8
MrinmoySonowal <mrinmoy.mly.sonowal@gmail.com>
2023-09-11 02:14:22 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Change directory to backend
 cd backend
 # Start backend server
-uvicorn coin_server:app --host localhost --port 8000 --workers 5 &
+uvicorn coin_server:app --host 0.0.0.0 --port 8000 --workers 5 &
 # Change directory to frontend
 cd ../client_website
 # Install dependencies with npm
